50b2e6e11579f19f41426240b8a0d5461b7ed574
StrongSan <tksakfnrla3@gmail.com>
2025-08-14 20:39:32 +0900

    day5 추가

---
+++ day5/day5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "숫자를 입력하세요: ";
+    cin >> num;
+
+    if (num % 2 == 0) {
+        cout << "짝수입니다.\n";
+    }
+    else {
+        cout << "홀수입니다.\n";
+    }
+
+
+    int score;
+    cout << "점수를 입력하세요: ";
+    cin >> score;
+
+    switch (score / 10) { // 90~100 → 9~10
+    case 10:
+    case 9:
+        cout << "A학점\n";
+        break;
+    case 8:
+        cout << "B학점\n";
+        break;
+    case 7:
+        cout << "C학점\n";
+        break;
+    case 6:
+        cout << "D학점\n";
+        break;
+    default:
+        cout << "F학점\n";
+    }
+
+    return 0;
+}
